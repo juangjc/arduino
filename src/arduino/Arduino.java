c480b923eaaea7e2f482c63f7edf59086be97723
Kalas <Kalas@Lenovo>
2019-08-23 12:36:30 -0500

    prueba

---
+++ src/arduino/Arduino.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arduino;
+
+/**
+ *
+ * @author Kalas
+ */
+public class Arduino {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    }
+    
+}
